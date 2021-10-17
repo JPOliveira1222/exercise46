8bc8a1235e0080965bab5366132658808f5c1cfa
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-16 20:08:40 -0400

    pseudocode for exercise 46

---
+++ src/main/java/baseline/solution46.java
@@ -0,0 +1,41 @@
+package baseline;
+
+public class solution46 {
+
+    /*
+
+    create a tester class
+    create an object that reads file
+    read the file
+    get the map
+    create an object of print report class and print it
+
+    create a class that reads file
+    create instance variables. scanner, file, hashmap
+    initialize instance variables
+    pass the path to the file
+
+    read file
+    use try and finally to avoid exceptions
+    read the whole line and split it by spaces
+    using a for loop for the length of the file
+    close the object
+    return the map
+
+    create a class that prints the report
+    create a variable map
+    initialize constructor
+    print histogram on screen
+
+    declare arrays to store keys and values
+    iterate map and store array
+    declare another array of strings and integer to store values
+    duplicate values
+    reverse sort values
+    sort key according to valuesarray and store in temp values
+
+    print final histogram
+
+
+     */
+}
